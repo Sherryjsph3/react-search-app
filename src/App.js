72b90527e19f228219184bbdbf392e45b624be66
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2023-01-23 22:06:21 -0500

    created search functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          `https://api.api-ninjas.com/v1/celebrity?name=${searchTerm}`,
+          {
+            headers: 
+            {
+              'X-Api-Key': '8kHbzjXIx8LS+/2+Q6kw0g==tawcyCUxTv50Cb6h'
+            }
+          }
+        );
+        const data = await res.json();
+        setSearchResults(data);
+        console.log(data);
+      } catch (error) {
+        setError(error);
+      }
+      setIsLoading(false);
+    }
+    if(searchTerm.length > 0) {
+      fetchData();
+    }
+  }, [searchTerm])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label>Search for a Nepo Baby:</label>
+        <input
+        type="text"
+        id="search"
+        value={searchTerm}
+        onChange={event => setSearchTerm(event.target.value)}
+        />
+      </form>
+      {error && <div>{error.message}</div>}
+
+      {isLoading ? (
+        <div>loading ...</div>
+        ) : (
+        searchResults.map(result => (
+      
+            <div key={result.id}>
+            <h2>{result.name}</h2>
+            <p>{result.min_height}</p>
+            <p>Nepo baby Net worth: {result.net_worth}</p>
+            </div> 
+       
+        ))
+      )}
     </div>
   );
 }
